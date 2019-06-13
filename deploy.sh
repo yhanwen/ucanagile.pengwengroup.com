8de5ba1d9aaa93e1af84276b08fad812f1ac4ee5
杨翰文 <yanghanwen@36kr.com>
2019-06-13 16:25:24 +0800

    build: commit changed files

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
-git commit -a -m 'build: commit changed files'
+git add .
+git commit -m 'build: commit changed files'
 npm run generate
 git checkout gh-pages
-git commit -a -m 'build: new version'
-npm version minor
 cp -r ./dist ./
 rm -rf dist
+git add .
+git commit -a -m 'build: new version'
+npm version minor
 git push origin gh-pages
 git checkout gh-pages-dev
