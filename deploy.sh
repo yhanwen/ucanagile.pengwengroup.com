dde0d0cf78d41d9bff8b05f5b4dc0ed77a477901
杨翰文 <yanghanwen@36kr.com>
2019-06-14 19:21:12 +0800

    build: commit changed files

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git add .
 git commit -m 'build: commit changed files'
 npm run generate
 git checkout gh-pages
-cp -r ./dist ./
+cp -r ./dist/ ./
 rm -rf dist
 git add .
 git commit -a -m 'build: new version'
