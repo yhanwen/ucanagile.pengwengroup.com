f535cdb9c3ec5cb007da6dd3d58f2d8615b4e4d5
杨翰文 <yanghanwen@36kr.com>
2020-02-24 16:50:26 +0800

    feat: 增加icp

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="common-page-wrapper">
     <nuxt class="common-content"/>
+    <div class="icp">&copy; 2011~2018 北京鹏文网络科技有限公司 | <a href="http://www.beian.miit.gov.cn/">京ICP备18056004号</a></div>
   </div>
 </template>
 
@@ -41,5 +42,20 @@ body, html {
   //   width: 100%;
   //   overflow: hidden;
   // }
+}
+.icp {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  font-size: 12px;
+  color: fadeout(#fff, 50);
+  text-align: center;
+  padding: 10px 0;
+  background: fadeout(#000, 50);
+  z-index: 100;
+  a {
+    color: fadeout(#fff, 30);
+  }
 }
 </style>
